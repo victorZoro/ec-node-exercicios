aaecf5b81a2c7155d46eccccba6aa2dab5af8395
Victor Gabriel <victorgabmariano@gmail.com>
2024-04-04 12:10:24 -0300

    fix: removing old directories from repository

--- exercicio3/query/category.query.js
+++
@@ -1,5 +0,0 @@
-const CATEGORY_QUERY = {
-    GET_CATEGORY_BY_ID: 'SELECT * FROM category WHERE id = ?',
-}
-
-export default CATEGORY_QUERY;
--- exercicio3/query/vehicle.query.js
+++
@@ -1,9 +0,0 @@
-const VEHICLES_QUERY = {
-    GET_VEHICLES: 'SELECT * FROM vehicle',
-    GET_VEHICLE_BY_ID: 'SELECT * FROM vehicle WHERE id = ?',
-    CREATE_VEHICLE: 'INSERT INTO vehicle(name, model, year, color, category, brand) VALUES(?, ?, ?, ?, ?, ?)',
-    UPDATE_VEHICLE: 'UPDATE vehicle SET name = ?, model = ?, year = ?, color = ?, category = ?, brand = ? WHERE id = ?',
-    DELETE_VEHICLE: 'DELETE FROM vehicle WHERE id = ?'
-}
-
-export default VEHICLES_QUERY;
