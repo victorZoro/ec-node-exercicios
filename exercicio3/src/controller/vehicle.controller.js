d9deb70df33bfdd4f3f8695e28fa42ed8b6fee35
Victor Gabriel <victorgabmariano@gmail.com>
2024-04-05 11:17:05 -0300

    feat: add all basic functions
    create, getAll, get, update and remove functions all added and working. The next step is to automatically add a new brand whenever a new value is presented.

--- exercicio3/src/controller/vehicle.controller.js
+++ exercicio3/src/controller/vehicle.controller.js
@@ -3,13 +3,21 @@ import { createVehicle } from "../model/createVehicle.model.js";
 import { connection } from "../../server.js";
 import { Response, HttpStatus } from "../model/feedback.model.js";
 import { retrieveVehicles } from "../model/retrieveVehicles.model.js";
+import { updateVehicle } from "../model/updateVehicle.model.js";
+import { removeVehicle } from "../model/removeVehicle.model.js";
 
 const vehicleController = {};
+
+let message = "";
+let output = {};
+let statusCode = 0;
+let status = "";
+
 
 const create = async (req, res) => {
   const { name, model, year, color, brand, category } = req.body;
 
-  const vehicle = {
+  const params = {
     name,
     model,
     year,
@@ -18,35 +26,40 @@ const create = async (req, res) => {
     category,
   };
 
-  if (validate(vehicle).isValid != true) {
+  if (validate(params).isValid != true) {
     return res.send(
       new Response(
         HttpStatus.BAD_REQUEST.code,
         HttpStatus.BAD_REQUEST.status,
-        validate(vehicle).message,
+        validate(params).message,
         null
       )
     );
   }
 
-  vehicle.year = Number(vehicle.year);
+  try {
+    output = await createVehicle(connection, params);
+    statusCode = HttpStatus.OK.code;
+    status = HttpStatus.OK.status;
+    message = "The vehicle was sucessfully added to database."
+  } catch (err) {
+    output = err;
+    statusCode = HttpStatus.INTERNAL_SERVER_ERROR.code,
+      status = HttpStatus.INTERNAL_SERVER_ERROR.status,
+      message = "There was an unexpected error while adding vehicle to database."
+  }
 
-  return res.send(
+  return res.status(statusCode).send(
     new Response(
-      HttpStatus.CREATED.code,
-      HttpStatus.CREATED.status,
-      "The vehicle was sucessfully created.",
-      createVehicle(connection, vehicle).vehicle
+      statusCode,
+      status,
+      message,
+      output
     )
-  )
+  );
 }
 
 const getAll = async (req, res) => {
-  
-  let message = "";
-  let output = {};
-  let statusCode = 0;
-  let status = "";
 
   try {
     output = await retrieveVehicles(connection, null);
@@ -57,10 +70,10 @@ const getAll = async (req, res) => {
     output = err;
     statusCode = HttpStatus.INTERNAL_SERVER_ERROR.code;
     status = HttpStatus.INTERNAL_SERVER_ERROR.status;
-    message = "There was an error retrieving the vehicles from the database";
+    message = "There was an unexpected error while retrieving the vehicles from the database";
   }
 
-  return res.send(
+  return res.status(statusCode).send(
     new Response(
       statusCode,
       status,
@@ -72,13 +85,118 @@ const getAll = async (req, res) => {
 
 const get = async (req, res) => {
 
+  const params = req.params;
+
+  if (!params.id) {
+    new Response(
+      HttpStatus.BAD_REQUEST.code,
+      HttpStatus.BAD_REQUEST.status,
+      `The id field is required. Didn't you mean https://{process.env.HOST}/vehicles ?`,
+      null
+    )
+  }
+
+  try {
+    output = await retrieveVehicles(connection, params);
+    statusCode = HttpStatus.OK.code;
+    status = HttpStatus.OK.status;
+    message = "The vehicle was sucessfully retrieved from database.";
+  } catch (err) {
+    output = err;
+    statusCode = HttpStatus.INTERNAL_SERVER_ERROR.code;
+    status = HttpStatus.INTERNAL_SERVER_ERROR.status;
+    message = "There was an unexpected error while retrieving the vehicle from the database";
+  }
+
+  return res.status(statusCode).send(
+    new Response(
+      statusCode,
+      status,
+      message,
+      output
+    )
+  );
 }
 
 const update = async (req, res) => {
+  const { name, model, year, color, brand, category } = req.body;
+  const id = req.params.id;
+  const params = {
+    id,
+    name,
+    model,
+    year,
+    color,
+    brand,
+    category,
+  };
 
+  if (validate(params).isValid != true) {
+    return res.send(
+      new Response(
+        HttpStatus.BAD_REQUEST.code,
+        HttpStatus.BAD_REQUEST.status,
+        validate(params).message,
+        null
+      )
+    );
+  }
+
+  try {
+    output = await updateVehicle(connection, params);
+    statusCode = HttpStatus.OK.code;
+    status = HttpStatus.OK.status;
+    message = "The vehicle was sucessfully updated in database."
+  } catch (err) {
+    output = err;
+    statusCode = HttpStatus.INTERNAL_SERVER_ERROR.code,
+      status = HttpStatus.INTERNAL_SERVER_ERROR.status,
+      message = "There was an unexpected error while updating vehicle in database."
+  }
+
+  return res.status(statusCode).send(
+    new Response(
+      statusCode,
+      status,
+      message,
+      output
+    )
+  );
 }
 
 const remove = async (req, res) => {
+
+  const params = req.params;
+
+  if (!params.id) {
+    new Response(
+      HttpStatus.BAD_REQUEST.code,
+      HttpStatus.BAD_REQUEST.status,
+      "The id field is required.",
+      null
+    )
+  }
+
+  try {
+    output = await removeVehicle(connection, params);
+    statusCode = HttpStatus.OK.code;
+    status = HttpStatus.OK.status;
+    message = "The vehicle was sucessfully removed from database.";
+  } catch (err) {
+    output = err;
+    statusCode = HttpStatus.INTERNAL_SERVER_ERROR.code;
+    status = HttpStatus.INTERNAL_SERVER_ERROR.status;
+    message = "There was an unexpected error while removing the vehicle from the database";
+  }
+
+  return res.status(statusCode).send(
+    new Response(
+      statusCode,
+      status,
+      message,
+      output
+    )
+  );
 
 }
 
