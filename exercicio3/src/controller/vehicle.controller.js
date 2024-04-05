9de47d537b7079bc087b9063b3a962e9822e96af
Victor Gabriel <victorgabmariano@gmail.com>
2024-04-05 10:02:32 -0300

    feat: update getAll function

--- exercicio3/src/controller/vehicle.controller.js
+++ exercicio3/src/controller/vehicle.controller.js
@@ -2,11 +2,11 @@ import { validate } from "./util/validation/vehicle.validation.js";
 import { createVehicle } from "../model/createVehicle.model.js";
 import { connection } from "../../server.js";
 import { Response, HttpStatus } from "../model/feedback.model.js";
-import { retrieveVehicles } from "../model/retrieveVehicles.js";
+import { retrieveVehicles } from "../model/retrieveVehicles.model.js";
 
 const vehicleController = {};
 
-const create = (req, res) => {
+const create = async (req, res) => {
   const { name, model, year, color, brand, category } = req.body;
 
   const vehicle = {
@@ -41,26 +41,44 @@ const create = (req, res) => {
   )
 }
 
-const getAll = (req, res) => {
+const getAll = async (req, res) => {
+  
+  let message = "";
+  let output = {};
+  let statusCode = 0;
+  let status = "";
+
+  try {
+    output = await retrieveVehicles(connection, null);
+    statusCode = HttpStatus.OK.code;
+    status = HttpStatus.OK.status;
+    message = "All vehicles retrieved from database.";
+  } catch (err) {
+    output = err;
+    statusCode = HttpStatus.INTERNAL_SERVER_ERROR.code;
+    status = HttpStatus.INTERNAL_SERVER_ERROR.status;
+    message = "There was an error retrieving the vehicles from the database";
+  }
+
   return res.send(
     new Response(
-      HttpStatus.OK.code,
-      HttpStatus.OK.status,
-      "All vehicles retrieved from database.",
-      retrieveVehicles(connection, null)
+      statusCode,
+      status,
+      message,
+      output
     )
   );
 }
 
-const get = (req, res) => {
+const get = async (req, res) => {
 
 }
 
-const update = (req, res) => {
+const update = async (req, res) => {
 
 }
 
-const remove = (req, res) => {
+const remove = async (req, res) => {
 
 }
 
--- exercicio3/src/model/retrieveVehicles.model.js
+++ exercicio3/src/model/retrieveVehicles.model.js
@@ -1,20 +1,26 @@
 import VEHICLES_QUERY from "../query/vehicle.query.js"
 
-export const retrieveVehicles = (connection, params) => {
+export const retrieveVehicles = async (connection, params) => {
+    if (!params) {
+        try {
+            const [results] = await connection.query(
+                VEHICLES_QUERY.GET_VEHICLES
+            );
+            return results;
+        } catch (err) {
+            throw err;
+        }
 
-    if(!params) {
-        return connection.mysql.query(
-            VEHICLES_QUERY.GET_VEHICLES,
-            function onResult(err, result) {
-                return err || result
-            }
-        )
     }
 
-    return connection.mysql.query(
-        VEHICLES_QUERY.GET_VEHICLE_BY_ID, [params.id],
-        function onResult(err, result) {
-            return err || result
-        }
-    )
+
+    try {
+        const [results] = await connection.query(
+            VEHICLES_QUERY.GET_VEHICLE_BY_ID,
+            [params.id]
+        );
+        return results;
+    } catch (err) {
+        throw err;
+    }
 }
