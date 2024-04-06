b51a9f5251dee3129d0a0fc9934436461c97be95
Victor Gabriel <vittimfkz@gmail.com>
2024-04-06 19:33:33 -0300

    fix: add brand routes
    TODO: Implement automation for creating new brands.
        Requisites:
            - A new brand must be created whenever a vehicle is added or updated in the database.
            - Whenever a new vehicle is added or updated, the brand must be checked in the database.
                - If the brand already exists: DO NOTHING!
                - If the brand does not exist: create a new brand.
            - The brand name must be unique.
            - The brand routes must be deleted after the implementation of the automation. They will not be necessary anymore.

--- exercicio3/server.js
+++ exercicio3/server.js
@@ -1,9 +1,21 @@
 import Fastify from "fastify";
 import dotenv from "dotenv";
-import { connect } from "./src/model/connection.model.js";
-import { vehicleController } from "./src/controller/vehicle.controller.js";
+import {connect} from "./src/model/connection.model.js";
+import {vehicleController} from "./src/controller/vehicle.controller.js";
+import {brandController} from "./src/controller/brand.controller.js";
 
 dotenv.config();
+
+/*
+TODO: Implement automation for creating new brands.
+    Requisites:
+        - A new brand must be created whenever a vehicle is added or updated in the database.
+        - Whenever a new vehicle is added or updated, the brand must be checked in the database.
+            - If the brand already exists: DO NOTHING!
+            - If the brand does not exist: create a new brand.
+        - The brand name must be unique.
+        - The brand routes must be deleted after the implementation of the automation. They will not be necessary anymore.
+*/
 
 export const fastify = Fastify({
     logger: true,
@@ -21,8 +33,13 @@ fastify.put('/vehicles/:id', async (req, res) => vehicleController.update(req, r
 
 fastify.delete('/vehicles/:id', async (req, res) => vehicleController.remove(req, res));
 
+// Brand Routes: must be deleted after the automation is implemented.
+fastify.get('/brands', async (req, res) => brandController.getAll(req, res));
+fastify.get('/brands/:id', async (req, res) => brandController.get(req, res));
+fastify.post('/brands', async (req, res) => brandController.create(req, res));
+
 try {
-    await fastify.listen({ port: process.env.API_PORT });
+    await fastify.listen({port: process.env.API_PORT});
 } catch (err) {
     fastify.log.error(err);
     process.exit(1);
