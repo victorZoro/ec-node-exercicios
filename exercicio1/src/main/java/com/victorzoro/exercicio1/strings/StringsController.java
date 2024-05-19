590e1ec71f33e5eedfffbeab98cbdd4650ce8378
Victor <victorgabmariano@gmail.com>
2024-05-19 13:47:21 -0300

    feat: add endpoint to delete a string from the list

--- exercicio1/src/main/java/com/victorzoro/exercicio1/strings/StringsController.java
+++ exercicio1/src/main/java/com/victorzoro/exercicio1/strings/StringsController.java
@@ -3,7 +3,9 @@ package com.victorzoro.exercicio1.strings;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,11 +23,16 @@ public class StringsController {
 
   @GetMapping("/strings")
   public List<IndexedStringsDto> getStrings() {
-    return stringsService.getStrings();
+    return this.stringsService.getStrings();
   }
 
   @PostMapping("/strings")
   public void addString(@RequestParam("string") String string) {
-    stringsService.addString(string);
+    this.stringsService.addString(string);
+  }
+
+  @DeleteMapping("/strings/{stringId}")
+  public void deleteString(@PathVariable("stringId") int stringId) {
+    this.stringsService.deleteString(stringId);
   }
 }
