e9d827c06b34064a69ff621ff45f75b44c1e6082
Victor <victorgabmariano@gmail.com>
2024-05-19 12:20:57 -0300

    feat: add enpoint that sums sent value with last value

--- exercicio1/src/main/java/com/victorzoro/exercicio1/soma/SomaService.java
+++ exercicio1/src/main/java/com/victorzoro/exercicio1/soma/SomaService.java
@@ -4,7 +4,26 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SomaService {
-  public int soma(int valor) {
-    return valor + 10;
+  private int lastValue;
+  private final int defaultValue = 10;
+
+  public SomaService() {
+    this.lastValue = this.defaultValue;
+  }
+
+  public int soma10(int valor) {
+    int resultado = valor + defaultValue;
+    this.setLastValue(resultado);
+    return resultado;
+  }
+
+  public int somaUltimoValor(int valor) {
+    int resultado = valor + lastValue;
+    this.setLastValue(resultado);
+    return resultado;
+  }
+
+  public void setLastValue(int value) {
+    this.lastValue = value;
   }
 }
